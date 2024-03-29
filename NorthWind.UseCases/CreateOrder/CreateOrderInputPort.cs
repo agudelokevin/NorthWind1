f0e1b4bfd44846a2dd8a9e8382799ad45026a53b
Kevin agudelo <kevinagude25@gmail.com>
2024-03-29 17:37:55 -0500

    CreateOrder

---
+++ NorthWind.UseCases/CreateOrder/CreateOrderInputPort.cs
@@ -0,0 +1,14 @@
+﻿using NorthWind.UseCasesDTOs.CreateOrder;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.CreateOrder
+{
+    public class CreateOrderInputPort:CreateOrderParams,IRequest<int>
+    {
+    }
+}
