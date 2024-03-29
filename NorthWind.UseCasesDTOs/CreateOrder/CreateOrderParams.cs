b74047dcb2e472a2a606a9ec0b5bcf32d0d6e14b
Kevin agudelo <kevinagude25@gmail.com>
2024-03-29 16:19:47 -0500

    CreateOrder

---
+++ NorthWind.UseCasesDTOs/CreateOrder/CreateOrderParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCasesDTOs.CreateOrder
+{
+    public class CreateOrderParams
+    {
+        public string CustomerId { get; set;}
+        public string ShipAddress {  get; set;}
+        public string ShipCity { get; set;}
+        public string ShipCountry { get; set;}
+        public string ShipPotalCode { get; set;}
+
+        public List<CreateOrderDetailParams> OrderDetails { get; set;}   
+    }
+}
