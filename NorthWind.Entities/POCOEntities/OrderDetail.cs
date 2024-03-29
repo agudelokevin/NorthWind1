35ee2f8ff282d4d28204b32eba22800768bb4562
Kevin agudelo <kevinagude25@gmail.com>
2024-03-29 13:20:27 -0500

    DataContext

--- NorthWind.Entities/POCOEntities/OrderDetail.cs
+++ NorthWind.Entities/POCOEntities/OrderDetail.cs
@@ -9,7 +9,7 @@ namespace NorthWind.Entities.POCOEntities
     public class OrderDetail
     {
         public int OrderId { get; set; }
-        public int ProductID { get; set; }
+        public int ProductId { get; set; }
         public decimal UnitPrice { get; set; }
         public short Quantity { get; set; }
         public Order Order { get; set; }
