427918ebaeafa462b989109eb0b648dd8ac5a0a0
Kevin agudelo <kevinagude25@gmail.com>
2024-04-12 23:24:04 -0500

    Filters

---
+++ NorthWind.WebExceptionsPresenter/Filters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthWind.Entities.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public static class Filters
+    {
+        public static void Register (MvcOptions options)
+        {
+            options.Filters.Add(new ApiExceptionFilterAttribute (
+                new Dictionary<Type, IExceptionHandle>
+                {
+                    {typeof(GeneralException),new GeneralExceptionHandler() },
+                    {typeof(ValidationException),new ValidationExceptionHandler() }
+                }
+                    ));
+        }
+    }
+}
